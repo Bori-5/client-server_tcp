f59adae54e7250fade3621b2f20dee67675c17f5
Bori-5 <58852833+Bori-5@users.noreply.github.com>
2020-03-01 20:38:34 +0100

    Nuove variabili e miglioramento multithreading
    Aggiunta di una variabile final per indicare il massimo numero di client cui è permesso connettersi.
    Completato il multithreading ma senza avere successo, i client non possono comunicare con il server, il programma si blocca al momento della richiesta di invio del messaggio.

--- src/socket/Server.java
+++ src/socket/Server.java
@@ -15,17 +15,28 @@ import java.net.*;
 public class Server {
 	
 	public static void main(String[] args) {
-		ServerSocket sSocket;
+		ServerSocket sSocket = null;
 		int porta = 3500;
 		boolean flag = false;
 		int time = 10; //tempo di attesa in secondi
 		int players_n = 0; //numero giocatori
-		Socket connessione; //!!!!!!!!da cambiare in serverEcho, Socket connessione non è più necessario come oggetto!!!!!!!!!!!!!!!!!!!!!!!!
-		ServerEcho echos;
-		
-		while (!flag && players_n < 3) { //fino a che non si sono connessi tutti i giocatori o non è avvenuta una eccezione
+		final int MAX_PLAYERS=1;
+		ServerEcho echo;
+		Thread[] trds = new Thread[MAX_PLAYERS];
+
+
+		try{
+			sSocket = new ServerSocket(porta);
+		}catch(BindException e){
+			System.out.println("Errore, un altro processo e' già in ascolto su questa porta\n"+e);
+			flag=true; //segnalo l'eccezione interrompendo il ciclo
+		}catch(Exception e2) {
+			flag=true; //segnalo l'eccezione generica interrompendo il ciclo
+		}
+
+
+		while (!flag && players_n < MAX_PLAYERS) { //fino a che non si sono connessi tutti i giocatori o non è avvenuta una eccezione
 			try{
-				sSocket = new ServerSocket(porta);
 				
 				System.out.println("in attesa della connessione...");
 				Countdown cd = new Countdown(sSocket,time); //istanzio un oggetto dedito al countdown
@@ -33,13 +44,13 @@ public class Server {
 				t.start(); //avvio il countdown
 				
 				
-				connessione = sSocket.accept(); //quando la connessione avviene
-				flag=true; //segnalo che la connessione è avvenuta
-				cd.setFlag(flag); //lo comunico anche al thread in esecuzione per fermare il countdown
+				echo = new ServerEcho(sSocket.accept()); //quando la connessione avviene
+				cd.setFlag(true); //lo comunico anche al thread in esecuzione per fermare il countdown
+				trds[players_n] = new Thread(echo);
+				trds[players_n].start(); //avvio il thread socket
 				System.out.println("Connessione stabilita");
-				
-				message_handler(connessione);
-				
+
+				players_n++;
 				
 			}catch(UnknownHostException e){
 				System.out.println("Errore DNS\n"+e);
@@ -53,36 +64,18 @@ public class Server {
 		}
 		
 		
-		if(flag){
+		if(players_n==MAX_PLAYERS){
+			//for(int c=0;c<players_n;c++){
 			try{
-				//!!!!!!!connessione da cambiare con un ciclo che chiude la connessione da tutti i thread!!!!!
-				connessione.close(); //chiudo il socket
+				sSocket.close(); //chiudo il socket
 				System.out.println("Connessione chiusa");
 			}catch(IOException e){
 				System.out.println("Error: Generic error\n"+e);
 			}
+
 			
 		}
 	}
 	
-	public static void message_handler(Socket connessione){
-		//da spostare sul thread
-		try{
-			InputStreamReader r=new InputStreamReader(System.in);  
-			BufferedReader buff=new BufferedReader(r);  
-			System.out.println("Enter your message: ");  
-			String msgout=buff.readLine();  
-			
-		// to send data to the client 
-		PrintStream ps = new PrintStream(connessione.getOutputStream());
-		ps.println(msgout);
-		//Once accepted 
-		InputStreamReader isr = new InputStreamReader(connessione.getInputStream());
-		BufferedReader br = new BufferedReader(isr);
-		String message = br.readLine();
-		System.out.println("Il client mi ha detto... " + message + " :D");
-		}catch(IOException e){
-			
-		}
-	}
+
 }
